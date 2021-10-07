9d8a49bf09eb043fe2bd43fd70c0e9c77576064b
Caio Stato <zombieiron9@gmail.com>
2021-10-07 17:42:15 -0300

    crawler 1.1

--- crawler/main.py
+++ crawler/main.py
@@ -10,36 +10,81 @@ import json
 
 def ficha(index):
     try:
-        WebDriverWait(web,2).until(
-            EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/main/div/div[2]/div/div[3]/table/tbody/tr['+str(index)+']/td[8]/a'))
-        )
-        elem = web.find_element_by_xpath('/html/body/div[2]/main/div/div[2]/div/div[3]/table/tbody/tr['+str(index)+']/td[8]/a').get_attribute('href')
-        web.execute_script("window.open('about:blank','secondtab');")
-        web.switch_to.window("secondtab")
-        web.get(elem)
+        try:
+            WebDriverWait(web,2).until(
+                EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/main/div/div[2]/div/div[3]/table/tbody/tr['+str(index)+']/td[8]/a'))
+            )
 
-        WebDriverWait(web,10).until(
-        EC.presence_of_element_located((By.XPATH,'//*[@id="estabContent"]/div/section/div[3]/div/div[2]'))
-        )
+        finally:
+            elem = web.find_element_by_xpath('/html/body/div[2]/main/div/div[2]/div/div[3]/table/tbody/tr['+str(index)+']/td[8]/a').get_attribute('href')
+            web.execute_script("window.open('about:blank','secondtab');")
+            web.switch_to.window("secondtab")
+            web.get(elem)
 
-        # htmlNome = web.find_element_by_id('nome')
-        # htmlNome = htmlNome.get_attribute('outerHTML')
-        # soup = BeautifulSoup(htmlNome,'html.parser')
-        # soup.find('input')['value']
-        # #Cep
-        # htmlCEP = web.find_element_by_id('cnpj')
-        # htmlCEP = htmlCEP.get_attribute('outerHTML')
-        # print(htmlCEP)
+        try:
+            WebDriverWait(web,10).until(
+            EC.presence_of_element_located((By.XPATH,'//*[@id="estabContent"]/div/section/div[3]/div/div[2]'))
+            )
 
-        # soup = BeautifulSoup(htmlCEP,'html.parser')
-        # soup.find('input')['value']
-        # cpnj = tree.xpath('//input[@id="cnpj"]/text()')
-        # print(cpnj)
+        # row 1 //*[@id="estabContent"]/div/section/div[3]/div/div[2]/div[1]/div/form/div[1]
+        # row 2 //*[@id="estabContent"]/div/section/div[3]/div/div[2]/div[1]/div/form/div[2]
 
+        #<input class="form-control input-sm" id="nome" type="text" ng-value="estabelecimento.noFantasia" readonly="" value="CENTRO DE SAUDE BARAO GERALDO">
+        # pos=116
 
+        finally:
+            row = []
+
+            for x in range(6):
+                y = web.find_element_by_xpath('//*[@id="estabContent"]/div/section/div[3]/div/div[2]/div[1]/div/form/div['+str(x+1)+']')
+                rowChild = y.find_element_by_tag_name('input')
+                rowChild = rowChild.get_attribute('outerHTML')
+                rowChild = rowChild.split(">")
+                for z in range(len(rowChild)):
+                    # find='CENTRO DE SAUDE'
+                    # posicao = int (rowChild[z].index(find) + len(find))
+                    # aux = rowChild[z]
+                    # rowChild[z]=''
+                    
+                    # for w in range(posicao,len(aux)):
+                    #     print(aux[w])
+                    #     while(aux[w]!='"'):
+                    #         rowChild[z] = rowChild[z] + aux[w]
+
+                    
+                    # find = 'CENTRO DE SAUDE'
+                    # aux= ''
+                    # pos = rowChild[z].index(find)
+
+                    # print(pos)
+
+                    # for w in range(pos,len(rowChild)):
+                    #     type(rowChild[z].index(w))
+                        
+
+                    
+
+                    
+
+                        
+
+                # htmlNome = web.find_element_by_id('nome')
+                # htmlNome = htmlNome.get_attribute('outerHTML')
+                # soup = BeautifulSoup(htmlNome,'html.parser')
+                # soup.find('input')['value']
+                # #Cep
+                # htmlCEP = web.find_element_by_id('cnpj')
+                # htmlCEP = htmlCEP.get_attribute('outerHTML')
+                # print(htmlCEP)
+
+                # soup = BeautifulSoup(htmlCEP,'html.parser')
+                # soup.find('input')['value']
+                # cpnj = tree.xpath('//input[@id="cnpj"]/text()')
+                # print(cpnj)
 
     except:
         web.close()
+        
 
 def fill():
     atendeSUSBtn = web.find_element_by_xpath('//*[@id="inlineRadio2"]')
@@ -68,9 +113,13 @@ def fill():
     sendBtn = web.find_element_by_xpath('/html/body/div[2]/main/div/div[2]/div/form[2]/div/button')
     sendBtn.click()
 
+try:
+    web = webdriver.Chrome(executable_path=binary_path)
+    web.get('http://cnes.datasus.gov.br/pages/estabelecimentos/consulta.jsp')
+except:
+    web.quit()
+    
 
-web = webdriver.Chrome(executable_path=binary_path)
-web.get('http://cnes.datasus.gov.br/pages/estabelecimentos/consulta.jsp')
 data={}
 data['unidade']=[]
 
