060a84ee891a5e6159d696573e10c80c4a2ed86d
caiostato <zombieiron9@gmail.com>
2022-04-09 01:37:14 -0300

    main file / new errors handlings

--- crawler/main.py
+++ crawler/main.py
@@ -1,10 +1,3 @@
-from asyncio.windows_events import NULL
-from lxml import html
-from pydoc import pager
-from unittest import defaultTestLoader
-#BeautifulSoup IMPORTS
-from bs4.element import PreformattedString, ProcessingInstruction, SoupStrainer
-from bs4 import BeautifulSoup
 #SELENIUM IMPORTS
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,31 +7,21 @@ from selenium.common.exceptions import TimeoutException
 import time
 import warnings
 #MODULES IMPORTS
-from modules.util.info import getInfo
-from modules.util.fill import fill
-from modules.factories.driver_factory import driverFactory
+from modules.util.info import get_info
+from modules.util.get_data import get_data
+from modules.util.fill import fill_keys
+from modules.classes.Driver import DriverCreator
 
-warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
-
-result = False
-
-web = driverFactory()
+driver = DriverCreator
+web = driver.factory_method()
 web.set_page_load_timeout(10)
 
-while result == False:
-    try:
-        print("get")
-        web.get('http://cnes.datasus.gov.br/pages/estabelecimentos/consulta.jsp')
-        WebDriverWait(web,2).until(
-        EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/main/div/div[2]/div/form[1]"))
-        )
-    except TimeoutException:
-        print("TimeoutException")
-        web.close()
-    else:
-        print("ELSE")
-        result = True
-        fill(web)
+web.get('http://cnes.datasus.gov.br/pages/estabelecimentos/consulta.jsp')
+WebDriverWait(web,2).until(
+EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/main/div/div[2]/div/form[1]"))
+)
+
+fill_keys(web)
 
 try:
     WebDriverWait(web,2).until(
@@ -47,19 +30,25 @@ try:
 except:
     pass
 
-# for page in range(7):
-#     page = page + 1
+for page in range(7):
+    page = page + 1
 
-#     if page < 7:
-#         for index in range(10):
-#             index = index + 1
-#             getInfo(web,index)
-#     else:
-#         for index in range(7):
-#             index = index + 1
-#             getInfo(web,index)
+    if page < 7:
+        for index in range(10):
 
-#     pageBtn = web.find_element_by_xpath('/html/body/div[2]/main/div/div[2]/div/div[3]/div/div/div/ul/li['+str(page+1)+']/a')
-#     pageBtn.click()
+            index = index + 1
+            repeat_result = False
 
-getInfo(web,1)
+            while repeat_result == False:
+                repeat_result = get_info(web,index)
+    else:
+        for index in range(7):
+
+            index = index + 1
+            repeat_result = False
+            
+            while repeat_result == False:
+                repeat_result = get_info(web,index)
+
+    pageBtn = web.find_element_by_xpath('/html/body/div[2]/main/div/div[2]/div/div[3]/div/div/div/ul/li['+str(page+1)+']/a')
+    pageBtn.click()
