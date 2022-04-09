6d47a2cb0df378b9bec33f4881caaefdec2e0d06
caiostato <zombieiron9@gmail.com>
2022-04-09 01:28:41 -0300

    new push function

--- crawler/modules/firebase/firebase.py
+++ crawler/modules/firebase/firebase.py
@@ -4,8 +4,9 @@ from firebase_admin import db
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv("../../../.env"))
-database_url = os.environ.get('DATABASE_URL')
+# load_dotenv(find_dotenv("../../../.env"))
+# database_url = os.environ.get('DATABASE_URL')
+database_url = "https://rising-mercury-327713-default-rtdb.firebaseio.com/"
 
 cred = credentials.Certificate("crawler/modules/firebase/firebaseSDK.json")
 firebase_admin.initialize_app(cred,{
@@ -14,22 +15,20 @@ firebase_admin.initialize_app(cred,{
 
 ref = db.reference('Unidade')
 
-def pushDB(data_list,dataServicos,dataEspecializacoes,coordsArray):
-        ref.push({
-                "nome": data_list[0],
-                "cep": '',
-                "logradouro": data_list[3],
-                "numero": data_list[4],
-                "bairro": data_list[5],
-                "telefone": data_list[9],
-                "tipoEstabelecimento": data_list[12],
-                "mantenedor": data_list[14],
-                "dialise":dataServicos[0],
-                "quimioRadio":dataServicos[1],
-                "hemoterapia":dataServicos[2],
-                "latitude":coordsArray[0],
-                "latitude":coordsArray[1],
-                
-        })
+def push_database(dict_data,dict_servicos,array_especializacoes):
 
-        #colocar os servicos
+        nome = dict_data.get('nome')
+        
+        for key,value in dict_data.items():
+                if(key != None and value != None):
+                        ref.child(nome).child(key).set(value)
+
+        for key,value in dict_servicos.items():
+                if(key != None and value != None):
+                        ref.child(nome).child("servicos").child(key).set(value)
+        serv_count = 1
+        for each in array_especializacoes:
+                if(each != None):
+                        ref.child(nome).child('especializacoes').child(str(serv_count)).set(each)
+                        serv_count = serv_count + 1
+
