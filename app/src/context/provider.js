12f3a024a3b964d3d78e928d47d1f6dba4669b1d
caiostato <zombieiron9@gmail.com>
2022-04-17 11:37:56 -0300

    marker context setup

---
+++ app/src/context/provider.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+export const MarkerContext = React.createContext()
+
+const Provider = (props) => {
+    
+    const [marker, setMarker] = useState()
+
+}
+
+export default Provider
