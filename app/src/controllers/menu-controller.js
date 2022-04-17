80e4143ea8b5eeb3f0f6e06967e0182dd8b566bd
caiostato <zombieiron9@gmail.com>
2022-04-17 11:37:31 -0300

    menu controller and models

---
+++ app/src/controllers/menu-controller.js
@@ -0,0 +1,14 @@
+import { React } from 'react'
+
+import Menu from '../components/menu/menu';
+
+const MenuController = () => {
+
+    return(
+        <div>
+            <Menu/>
+        </div>
+    )
+}
+
+export default MenuController;
