6f28ceb3d49da47f6ff45803f7358c1cf2175eb5
caiostato <zombieiron9@gmail.com>
2022-04-23 19:54:11 -0300

    services / api to leaflet

---
+++ app/src/services/osm-provider.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    maptiler: {
+        url:
+            "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=ANIctFtmftpay3bFPJEn",
+        attribution:
+            '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    },
+};
